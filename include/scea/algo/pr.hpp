47c8e50d4f3c0d100a5879a1b794c69a34be4b7c
anubhavgoel26 <anubhavgoel026@gmail.com>
2024-04-09 02:53:13 +0000

    Added parallel version of page rank Added serial version of betwenness centrality Added plumbing in edit_scalability to make them work

---
+++ include/scea/algo/pr.hpp
@@ -0,0 +1,82 @@
+// SPDX-License-Identifier: BSD-2-Clause
+// Copyright (c) 2023. University of Texas at Austin. All rights reserved.
+
+#pragma once
+
+#include <vector>
+#include <cmath>
+#include <atomic>
+#include <algorithm>
+
+#include "algo_interface.hpp"
+#include "galois/LargeArray.h"
+#include "galois/AtomicHelpers.h"
+#include "galois/Reduction.h"
+
+namespace scea::algo {
+
+class PageRank : public Algo {
+  static constexpr double DAMPING_FACTOR = 0.85;
+  static constexpr int MAX_ITERATIONS = 100;
+  static constexpr double TOLERANCE = 1.0e-4;
+
+public:
+  PageRank() = default;
+
+  static void compute(scea::graph::MutableGraph& g) {
+    const uint64_t numNodes = g.size();
+    std::vector<std::atomic<double>> newRank(numNodes);
+    std::vector<double> rank(numNodes, 1.0 / numNodes);
+    std::vector<unsigned> out_degrees(numNodes, 0);
+
+    galois::do_all(
+      galois::iterate(0ul, numNodes),
+      [&](uint64_t i) {
+        g.for_each_edge(i, [&](uint64_t const&) { out_degrees[i]++; });
+      },
+      galois::no_stats(), galois::loopname("ComputeOutDegrees"), galois::steal()
+    );
+
+    for (int iter = 0; iter < MAX_ITERATIONS; ++iter) {
+      std::for_each(newRank.begin(), newRank.end(), [](std::atomic<double>& n) { n.store(0.0); });
+
+      galois::do_all(
+        galois::iterate(0ul, numNodes),
+        [&](uint64_t i) {
+          g.for_each_edge(i, [&](uint64_t dst) {
+            galois::atomicAdd(newRank[dst], rank[i] / out_degrees[i]);
+          });
+        },
+        galois::no_stats(), galois::loopname("DistributeContributions"), galois::steal()
+      );
+
+      galois::do_all(
+        galois::iterate(0ul, numNodes),
+        [&](uint64_t i) {
+          newRank[i].store((1.0 - DAMPING_FACTOR) / numNodes + DAMPING_FACTOR * newRank[i].load(std::memory_order_relaxed), std::memory_order_relaxed);
+        },
+        galois::no_stats(), galois::loopname("ComputeNewRanks"), galois::steal()
+      );
+
+      galois::GAccumulator<double> diff;
+      galois::do_all(
+        galois::iterate(0ul, numNodes),
+        [&](uint64_t i) {
+          diff += std::abs(newRank[i].load(std::memory_order_relaxed) - rank[i]);
+          rank[i] = newRank[i].load(std::memory_order_relaxed);
+        },
+        galois::no_stats(), galois::loopname("CheckConvergenceAndUpdate"), galois::steal()
+      );
+
+      if (diff.reduce() < TOLERANCE) {
+        break;
+      }
+    }
+  }
+
+  void operator()(scea::graph::MutableGraph& g) override {
+    compute(g);
+  }
+};
+
+} // namespace scea::algo
